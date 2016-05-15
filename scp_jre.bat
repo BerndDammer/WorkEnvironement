96b05774fd6efbcb3a22e0935d8c99d5e4787663
BerndDammer <BerndDammer@web.de>
2016-05-15 09:10:01 +0200

    scp

---
+++ scp_jre.bat
@@ -0,0 +1,5 @@
+set PATH=%PATH%;c:\PortableApps\WinSCP
+
+rem WinSCP alarm@alarmpi /command "cd /" /command ls
+winscp.com /command "open sftp://alarm:alarm@alarmpi/" "put jre_b1 /home/alarm" 
+pause
